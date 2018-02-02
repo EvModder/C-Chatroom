12f5c444bb5d56a74a36e073c671376bb8d4a520
Chase Elander <chaseelander@gmail.com>
2018-02-01 21:46:25 -0600

    create command, join command, list command

--- chatroom_manager.c
+++ chatroom_manager.c
@@ -8,13 +8,83 @@
 
 int REPLY_SIZE = sizeof(struct Reply);
 int SERVER_PORT = 59254;
+int START_PORT = 4000;
 
-void create_chatroom(int port){
+void create_chatroom(int port, char * name){
+	
+	char buff[526];
+
+	for (auto const& t : (* chatroom_map)){ //check that a chatroom with this
+		if(strcmp(t.first.c_str(), name) == 0) { //  name does not already exists
+			//it does already exist
+			strcpy(buff, FAILURE_ALREADY_EXISTS.c_str());
+			send(port, buff, 526, 0); //Send the failure
+			return;
+		}
+	}
+
+	//The chatroom doesn't exist yet
+
+	//initialize new chatroom and push into chatroom_map
+	int chat_socket_temp = passiveTCPConnection(chat_port_temp);
+	struct Chatroom * chat = new Chatroom();
+	chat->socket = chat_socket_temp;
+	chat->port = chat_port_temp;
+	chat->num_member = 0;
+	chat->name = string(name);
+	(*chatroom_map)[name] = chat;
+
+	chat_port_temp++; //iterate chat_port_temp to ensure unique ports
+
+	string reply_temp = SUCCESS + " 0 " + to_string(chat->port);
+	strcpy(buff, reply_temp.c_str());
+	send(sd, buff, 526, 0); //Send it back to client
+
+/*                            Original Create chatroom code
 	char str[12];
 	sprintf(str, "%d", port);
 	char *argv[]={"server.o", str, NULL};
 	printf("Starting chatroom on port %d\n", port);
 	if(fork() == 0) execv("./server.o", argv);
+	*/
+}
+
+struct Chatroom {
+	int socket;
+	int port;
+	int num_members;
+	string name;
+	pthread_t * thread;
+	vector<int> clients;
+};
+
+map<string, struct Chatroom * > *chatroom_map;
+
+int chat_port_temp = START_PORT;
+
+void list_command(){
+	string reply_temp = SUCCESS + " 0 0 ";
+	for (auto const& t : (*chatroom_map)){
+		reply_temp = reply_temp + t.first + ",";
+	}
+
+	//Send it back to Client
+	return;
+}
+void join_command(char * name){
+	string reply_temp = "";
+
+	auto t = (chatroom_map).find(string(name));//verify it exists
+	if( t == (*chatroom_map).end()){
+		reply_temp = "FAILURE_NOT_EXISTS -1 -1";
+	}
+	else{
+		struct Chatroom * ch = (*chatroom_map)[string(name)];
+			ch ->num_member +=1;
+			reply_temp = "SUCCESS " + to_string(c->num_member) + " " + to_string(c->port);
+	}
+
+	//Send back to client (maybe with write command?)
 }
 
 /*
@@ -29,10 +99,11 @@ void run_command(char* command, struct Reply* reply){
 
 	// TODO: Implement these
 	if(startswith(command, "LIST")){
-
+		list_command();
 	}
 	else if(startswith(command, "JOIN ")){
 		// substr(5) to get chatroom name
+		join_command(substr(5));
 	}
 	else if(startswith(command, "CREATE ")){
 		//TODO: idk something for sure
